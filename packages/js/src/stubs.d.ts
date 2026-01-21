9da52343127994cb0ed5fd08ba4c6e0d2bf98928
Dmytro Zelenetskyi <dmitry.zelenetskiy@gmail.com>
2026-01-21 16:56:16 +0100

    Update

--- packages/js/src/stubs.d.ts
+++ packages/js/src/stubs.d.ts
@@ -1,9 +1,1 @@
 declare module "@eslint-community/eslint-plugin-eslint-comments";
-
-declare module "module" {
-  export function createRequire(url: string | URL): (id: string) => any;
-}
-
-interface ImportMeta {
-  url: string;
-}
